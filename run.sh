f18ffd68d07e3a554222021030eb0a8674d736fd
Nithish Reddy <nithishreddyb2@gmail.com>
2025-09-03 00:31:37 +0530

    modified run.sh

--- run.sh
+++ run.sh
@@ -1,23 +1,12 @@
-pip install gdown
-
-
+pip3 install gdown
 
 gdown --fuzzy https://drive.google.com/file/d/1nYKpWzuQCa-ck-DUSUVLQ8-7tZjLGAol/view?usp=drive_link
 
 gdown --fuzzy https://drive.google.com/file/d/1ScnKWqGgx_j7cYNYgJZp3SltM_pRxx_b/view?usp=drive_link
 
-gdown --fuzzy https://drive.google.com/file/d/19-wbsK7D0J0-9lvcj8d68_y6Pgod9kHH/view?usp=sharing
 
 unzip archive.zip
 
 
 unzip 'archive (1).zip'
-
-
-unzip file.zip
-
-
-
-pip install -r requirements.txt
-
-rm -r archive.zip 'archive (1).zip' file.zip
+s
